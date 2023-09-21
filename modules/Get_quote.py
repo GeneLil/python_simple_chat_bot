2dbef7f99785ed8563afc6d86720628d3732d248
AminAli-DS <66701535+AminAli-DS@users.noreply.github.com>
2023-09-21 06:30:56 +0300

    Add files via upload

---
+++ modules/Get_quote.py
@@ -0,0 +1,11 @@
+import requests
+
+def get_quote():
+    url = 'https://api.quotable.io/random'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return f'"{data["content"]}" - {data["author"]}'
+    else:
+        return "Проблема при отриманні цитати"
+
